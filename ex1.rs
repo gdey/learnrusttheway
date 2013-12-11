76947cc4d5bd41c801668cc4afe5890694db37e1
Gautam Dey <gautam@tealium.com>
2013-12-10 21:37:53 -0800

    Did drill two and three.

--- ex1.rs
+++ ex1.rs
@@ -1,9 +1,11 @@
 fn main() {
    println("Hello World!");
+/*
    println( "I like typing this.");
    println( "This is fun.");
    println( "Ya! Printing.");
    println( "I'd much rather you 'not'.");
    println( "I \"said\" do not touch this.");
    println( "Yay, I get to write one more line.");
+*/
 }
