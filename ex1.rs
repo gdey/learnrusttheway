85749edddf38706891e03e150858fa595bbfb3ed
Gautam Dey <gautam@tealium.com>
2013-12-10 21:46:49 -0800

    Compleated ex2

--- ex1.rs
+++ ex1.rs
@@ -1,3 +1,4 @@
+/* _*_ coding: utf-8 filetype: rust _*_ */
 fn main() {
    println("Hello World!");
 /*
---
+++ ex2.rs
@@ -0,0 +1,17 @@
+/* A comment, this is so you can read your program later. */
+/* 
+In Rust there like c/c++ there are two type of comment delimiters:
+     For block comments you use '/' followed by a '*' and to close it you do the
+     opposite; a '*' followed by a '/'. Comments can all be start with two '/'.
+     And will go to the end of the line. 
+*/
+
+fn main() {
+    println("I could have code like this."); // and the commend after it is ignored.
+
+/* 
+   You can also use a comment to "disable" or "commend out" a piece of code:
+   println("This won't run.");
+*/
+   println("This will run."); 
+}
