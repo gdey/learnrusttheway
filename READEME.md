6e29109fd46ad3066a90b96dec238528fb025792
Gautam Dey <gautam@tealium.com>
2013-12-10 21:33:06 -0800

    Init commit.

---
+++ READEME.md
@@ -0,0 +1,3 @@
+# Quick Rust examples of Learn Python the Hard Way.
+
+I'm just going through the book, but doing all the work in rust.
---
+++ ex1.rs
@@ -0,0 +1,8 @@
+fn main() {
+   println("Hello World!");
+   println( "I like typing this.");
+   println( "This is fun.");
+   println( "Ya! Printing.");
+   println( "I'd much rather you 'not'.");
+   println( "I \"said\" do not touch this.");
+}
